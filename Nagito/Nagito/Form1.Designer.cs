a20c7b1c3446a8a2fdebdbea834bade2c423fa66
kenyh <71294924+xKenyh@users.noreply.github.com>
2021-08-16 01:10:45 +0200

    Add files via upload

---
+++ Nagito/Nagito/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿
+namespace Nagito
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.LoopCheckbox = new System.Windows.Forms.CheckBox();
+            this.SoundList = new System.Windows.Forms.ListBox();
+            this.Remove = new System.Windows.Forms.Button();
+            this.Add = new System.Windows.Forms.Button();
+            this.ClearList = new System.Windows.Forms.Button();
+            this.AddFolder = new System.Windows.Forms.Button();
+            this.Play = new System.Windows.Forms.Button();
+            this.Stop = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(189)))), ((int)(((byte)(255)))), ((int)(((byte)(15)))));
+            this.panel1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.panel1.Location = new System.Drawing.Point(-11, -7);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(825, 23);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("ProggyCleanTT", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(205)))), ((int)(((byte)(255)))), ((int)(((byte)(53)))));
+            this.button1.Location = new System.Drawing.Point(760, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(28, 29);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("ProggyCleanTT", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(205)))), ((int)(((byte)(255)))), ((int)(((byte)(53)))));
+            this.button3.Location = new System.Drawing.Point(726, 22);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(28, 29);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "-";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Nagito.Properties.Resources.R;
+            this.pictureBox1.Location = new System.Drawing.Point(523, 251);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(291, 203);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // LoopCheckbox
+            // 
+            this.LoopCheckbox.AutoSize = true;
+            this.LoopCheckbox.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.LoopCheckbox.Font = new System.Drawing.Font("ProggyCleanTT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LoopCheckbox.ForeColor = System.Drawing.Color.White;
+            this.LoopCheckbox.Location = new System.Drawing.Point(12, 419);
+            this.LoopCheckbox.Name = "LoopCheckbox";
+            this.LoopCheckbox.Size = new System.Drawing.Size(54, 17);
+            this.LoopCheckbox.TabIndex = 5;
+            this.LoopCheckbox.Text = "Loop";
+            this.LoopCheckbox.UseVisualStyleBackColor = true;
+            // 
+            // SoundList
+            // 
+            this.SoundList.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(30)))));
+            this.SoundList.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.SoundList.Font = new System.Drawing.Font("ProggyCleanTT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.SoundList.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(209)))), ((int)(((byte)(255)))), ((int)(((byte)(72)))));
+            this.SoundList.FormattingEnabled = true;
+            this.SoundList.Location = new System.Drawing.Point(12, 37);
+            this.SoundList.Name = "SoundList";
+            this.SoundList.Size = new System.Drawing.Size(492, 249);
+            this.SoundList.TabIndex = 7;
+            // 
+            // Remove
+            // 
+            this.Remove.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(30)))));
+            this.Remove.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Remove.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(30)))));
+            this.Remove.FlatAppearance.BorderSize = 0;
+            this.Remove.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Remove.Font = new System.Drawing.Font("ProggyCleanTT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Remove.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(255)))), ((int)(((byte)(0)))));
+            this.Remove.Location = new System.Drawing.Point(510, 66);
+            this.Remove.Name = "Remove";
+            this.Remove.Size = new System.Drawing.Size(75, 23);
+            this.Remove.TabIndex = 8;
+            this.Remove.Text = "Remove";
+            this.Remove.UseVisualStyleBackColor = false;
+            this.Remove.Click += new System.EventHandler(this.Remove_Click);
+            // 
+            // Add
+            // 
+            this.Add.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(30)))));
+            this.Add.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Add.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(30)))));
+            this.Add.FlatAppearance.BorderSize = 0;
+            this.Add.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Add.Font = new System.Drawing.Font("ProggyCleanTT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Add.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(255)))), ((int)(((byte)(0)))));
+            this.Add.Location = new System.Drawing.Point(510, 37);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(75, 23);
+            this.Add.TabIndex = 9;
+            this.Add.Text = "Add";
+            this.Add.UseVisualStyleBackColor = false;
+            this.Add.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // ClearList
+            // 
+            this.ClearList.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(30)))));
+            this.ClearList.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ClearList.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(30)))));
+            this.ClearList.FlatAppearance.BorderSize = 0;
+            this.ClearList.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ClearList.Font = new System.Drawing.Font("ProggyCleanTT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ClearList.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(255)))), ((int)(((byte)(0)))));
+            this.ClearList.Location = new System.Drawing.Point(591, 37);
+            this.ClearList.Name = "ClearList";
+            this.ClearList.Size = new System.Drawing.Size(75, 52);
+            this.ClearList.TabIndex = 12;
+            this.ClearList.Text = "Clear list";
+            this.ClearList.UseVisualStyleBackColor = false;
+            this.ClearList.Click += new System.EventHandler(this.ClearList_Click);
+            // 
+            // AddFolder
+            // 
+            this.AddFolder.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(30)))));
+            this.AddFolder.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.AddFolder.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(30)))));
+            this.AddFolder.FlatAppearance.BorderSize = 0;
+            this.AddFolder.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.AddFolder.Font = new System.Drawing.Font("ProggyCleanTT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.AddFolder.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(255)))), ((int)(((byte)(0)))));
+            this.AddFolder.Location = new System.Drawing.Point(510, 95);
+            this.AddFolder.Name = "AddFolder";
+            this.AddFolder.Size = new System.Drawing.Size(75, 52);
+            this.AddFolder.TabIndex = 13;
+            this.AddFolder.Text = "Add Folder";
+            this.AddFolder.UseVisualStyleBackColor = false;
+            this.AddFolder.Click += new System.EventHandler(this.AddFolder_Click);
+            // 
+            // Play
+            // 
+            this.Play.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(30)))));
+            this.Play.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Play.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(30)))));
+            this.Play.FlatAppearance.BorderSize = 0;
+            this.Play.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Play.Font = new System.Drawing.Font("ProggyCleanTT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Play.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(255)))), ((int)(((byte)(0)))));
+            this.Play.Location = new System.Drawing.Point(12, 292);
+            this.Play.Name = "Play";
+            this.Play.Size = new System.Drawing.Size(75, 23);
+            this.Play.TabIndex = 14;
+            this.Play.Text = "Play";
+            this.Play.UseVisualStyleBackColor = false;
+            this.Play.Click += new System.EventHandler(this.Play_Click);
+            // 
+            // Stop
+            // 
+            this.Stop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(30)))));
+            this.Stop.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Stop.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(30)))));
+            this.Stop.FlatAppearance.BorderSize = 0;
+            this.Stop.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Stop.Font = new System.Drawing.Font("ProggyCleanTT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Stop.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(255)))), ((int)(((byte)(0)))));
+            this.Stop.Location = new System.Drawing.Point(103, 292);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(75, 23);
+            this.Stop.TabIndex = 15;
+            this.Stop.Text = "Stop";
+            this.Stop.UseVisualStyleBackColor = false;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(25)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.Play);
+            this.Controls.Add(this.AddFolder);
+            this.Controls.Add(this.ClearList);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.Remove);
+            this.Controls.Add(this.SoundList);
+            this.Controls.Add(this.LoopCheckbox);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(184)))), ((int)(((byte)(255)))), ((int)(((byte)(87)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Nagito Mic Tool";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.CheckBox LoopCheckbox;
+        private System.Windows.Forms.ListBox SoundList;
+        private System.Windows.Forms.Button Remove;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.Button ClearList;
+        private System.Windows.Forms.Button AddFolder;
+        private System.Windows.Forms.Button Play;
+        private System.Windows.Forms.Button Stop;
+    }
+}
+
---
+++ Nagito/Nagito/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.WebRequestMethods;
+
+namespace Nagito
+{
+    public partial class Form1 : Form
+    {
+        SoundPlayer Sound;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // Nagito main code
+
+        // Add music
+        private void Add_Click(object sender, EventArgs e)
+        {
+            // Open File
+            string pathFile = string.Empty;
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.Filter = "WAV|*.wav";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                pathFile = openFileDialog.FileName;
+            }
+
+            SoundList.Items.Add(pathFile.ToString());
+        }
+
+        // Add folder
+        private void AddFolder_Click(object sender, EventArgs e)
+        {
+            // Open File
+            string filepath = string.Empty;
+            FolderBrowserDialog openFolderDialog = new FolderBrowserDialog();
+
+            if (openFolderDialog.ShowDialog() == DialogResult.OK)
+            {
+                filepath = openFolderDialog.SelectedPath;
+            }
+
+            foreach (string file in Directory.GetFiles(filepath, "*.wav", SearchOption.AllDirectories))
+            {
+                SoundList.Items.Add(file);
+            }
+        }
+
+
+            // Play music
+            private void Play_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                object SelectedSound = SoundList.SelectedItem;
+
+                Sound = new SoundPlayer(@SelectedSound.ToString());
+
+                if (LoopCheckbox.Checked == true)
+                {
+                    Sound.PlayLooping();
+                }
+
+                else
+                {
+                    Sound.Play();
+                }
+            }
+
+            catch
+            {
+                MessageBox.Show("No file selected");
+            }
+        }
+
+
+        // Stop Music
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            Sound.Stop();
+        }
+
+        // Remove Music
+        private void Remove_Click(object sender, EventArgs e)
+        {
+            int SelectedSound = SoundList.SelectedIndex;
+
+            if(SelectedSound != -1)
+            {
+                SoundList.Items.RemoveAt(SelectedSound);
+            }
+        }
+
+
+        // Clear list
+        private void ClearList_Click(object sender, EventArgs e)
+        {
+            SoundList.Items.Clear();
+        }
+
+
+
+        // Close Button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+
+        // Move Window
+        int m, mx, my;
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            m = 0;
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (m == 1)
+            {
+                this.SetDesktopLocation(MousePosition.X - mx, MousePosition.Y - my);
+            }
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            m = 1;
+            mx = e.X;
+            my = e.Y;
+        }
+    }
+}
